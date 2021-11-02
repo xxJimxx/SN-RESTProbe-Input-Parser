2927ae065c66f41fd89bdf34d0c4abd8368d35fd
Jim Myrah <xxJimxx@users.noreply.github.com>
2021-11-02 15:38:09 -0500

    Create ECC_RESTProbe_Await.js

---
+++ ECC_RESTProbe_Await.js
@@ -0,0 +1,32 @@
+//Async Business Rule on ecc_queue insert 
+(function executeRule(current, previous /*null when async*/ ) {
+
+    var probe = new ECC_RESTProbe();
+    var result = probe.toObject(current);
+    if (!result || !result.request || !result.request.headers || !result.request.headers["SN.PROCESS_ID"]) {
+        return;
+    }
+
+    var api = null;
+    try {
+        var grAwait = new GlideRecord('u_api_await');
+        grAwait.addQuery('u_active', true);
+        grAwait.addQuery('u_process_id', result.request.headers["SN.PROCESS_ID"]);
+        grAwait.query();
+        if (grAwait.next()) {
+            var prop = {
+                scriptInclude: grAwait.u_script_include.name.toString(),
+                method: grAwait.getValue('u_method_name')
+            };
+            api = GlideEvaluator.evaluateString('new ' + prop.scriptInclude + '()');
+            api[prop.method](result);
+        } 
+    } 
+	catch (ex) {
+        if (api) {
+            gs.log(ex.message, api.type);
+        } else {
+            gs.log(ex.message, 'WMT ECC RESTProbe Await');
+        }
+    }
+})(current, previous);
